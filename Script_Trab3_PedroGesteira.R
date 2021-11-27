5dd2eb2ba8bd68ba0de041f7058861d8485542d1
Pedro Gesteira <pedro06g@gmail.com>
2021-11-27 15:46:33 -0300

    Questão 1 revisada e questão 2 iniciada.
    - Pensar nos NAs
    
    - Terminar as medidas e ver como transformar elas numa tabela de estatísticas

--- Script_Trab3_PedroGesteira.R
+++ Script_Trab3_PedroGesteira.R
@@ -1,18 +1,20 @@
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(tidyverse, haven, stargazer)
+pacman::p_load(tidyverse, haven, stargazer, fixest, MASS, modelsummary)
 
-##Questão 1----
+###Questão 1----
+
+##Arrumando a base
 
 dir <- getwd()
 
-censo <- read_dta("data/censo_2000_rio.dta") %>%  #Leitura da base
+censo <- read_dta(paste(dir, "/data/censo_2000_rio.dta", sep = "")) %>%  #Leitura da base
   filter(v0103 == 3304557)   #Delimitando para englobar apenas Rio cidade
   
 #Selecionando as variáveis de interesse
 
-censo <- select(c(v0401, v4752, v4300, v0408, v0439, v4513, v0453, v4620, v4621, v4622))
+censo <- censo %>% select(c(v0401, v4752, v4300, v0408, v0439, v4513, v0453, v4620, v4621, v4622))
 
-Renomeando as variáveis
+#Renomeando as variáveis
 
 names(censo) <- c("d_mulher", "idade", "educa", "branco", "trab", "salario", "horas", "n_fil", "n_fil_o", "n_fil_a")
 
@@ -33,3 +35,20 @@ censo <- censo %>% filter(d_mulher == 1 & idade<=60 & idade>=18)
 #Relocando as colunas
 
 censo <- censo[c(1:2, 11, 3:7, 12:13, 8:10, 14)]
+
+##Realizando as medidas pedidas pela questão
+
+medidas <- list("Média" = sapply(censo, mean, na.omit = TRUE), 
+                "Mínimo" = sapply(censo, min, na.omit = TRUE))
+
+###Questão 2----
+
+#Regressões pedidas
+
+q2reg1 <- feols(trab ~ d_fil, data = censo, se = "standard")
+
+q2reg2 <- feols(trab ~ d_fil, data = censo, se = "hetero")
+
+#Tabela com as regressões
+
+q2tab1 <- modelsummary(list("Simples" = q2reg1, "Robusto" = q2reg2))
